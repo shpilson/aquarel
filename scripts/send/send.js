a20b8017148d5870363dec89ef12828da9b75d82
shpilson <kovalenko.jora@gmail.com>
2021-05-14 15:58:31 +0200

    update

--- scripts/send/send.js
+++ scripts/send/send.js
@@ -9,20 +9,14 @@ sendSelector.insertAdjacentHTML('afterend', `
         <ul class="contacts__list">
           
             <form>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <div class="form-group">>
+                  <input type="text" class="form-control" id="client_name" aria-describedby="clientName" placeholder="Ваше имя">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
-                <div class="form-group form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Оставить заявку</button>
               </form>
 
         </ul>
