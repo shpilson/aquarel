a4bbd9f7488d4bbeff8db7305bf2dab1241afb47
shpilson <kovalenko.jora@gmail.com>
2021-05-14 15:48:19 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,11 @@ import pages from './config/conditions.js' // Константы страниц
 import catalog from './catalog/catalog.js'; // Категория товаров
 import cart from './cart/cart.js'; // Смена кнопки "Купить" на "В корзину"
 import map from './map/map.js'; // Карта и контакты
+import send from './send/send.js'; // Форма обратной связи
 
 if (pages.main) {
     catalog();
     cart();
     map();
+    send();
   }
---
+++ scripts/send/send.js
@@ -0,0 +1,40 @@
+const sendSelector = document.querySelector(".custom_section")
+
+function send() {
+sendSelector.insertAdjacentHTML('afterend', `
+<div class="container">
+    <div class="contact-with-map row">
+        <div class="contacts col-md-4 col-12">
+        <h5 class="contacts__title">Остались вопросы?</h5>
+        <ul class="contacts__list">
+          
+            <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+
+        </ul>
+        </div>
+        <div class="contact__map col-md-8 col-12">
+        <span>text</span>
+        </div>
+    </div>
+</div>`)
+}
+
+export default send;
+
+
+
