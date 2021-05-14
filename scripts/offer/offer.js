1f883782fbf10c4a44cd5d6b3e9e85dee4a8f7c2
shpilson <kovalenko.jora@gmail.com>
2021-05-14 12:56:50 +0200

    update

--- scripts/offer/offer.js
+++ scripts/offer/offer.js
@@ -5,7 +5,7 @@ offerMain.insertAdjacentHTML('afterbegin', `
 <div class="container">
 
 <div class="contact-with-map row">
-<img src="https://shpilson.github.io/aquarel/styles/images/offer.jpeg"></img>
+<img src="https://shpilson.github.io/aquarel/styles/images/offer.jpeg" style="border: 3px solid rgba(142, 142, 142, 0.46);">
 </div>
 
 </div>`)
