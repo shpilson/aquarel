669eda40b1dea87bd4aab557b90f5c8aa94fd788
shpilson <kovalenko.jora@gmail.com>
2021-05-15 15:45:47 +0200

    update

--- scripts/widgets/header.js
+++ scripts/widgets/header.js
@@ -1,7 +1,7 @@
 const widgetSelector = document.querySelector("#undefined-sticky-wrapper > header > div.header-wrapper > div.container-fluid > div.header_widgets");
 
 function header() {
-widgetSelector.insertAdjacentHTML('afterbegin',`
+widgetSelector.insertAdjacentHTML('beforeend',`
     <div class="header_test">
         <a class="d-none d-lg-block" href="https://store108758.sellavi.com/login/" role="link">Авторизация</a>
         <a class="d-none d-sm-block d-lg-none" href="https://store108758.sellavi.com/login/" role="link"><i class="far fa-user"></i></a>
