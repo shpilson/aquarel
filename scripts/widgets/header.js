c46c4d59f0d85c8a7d04e6dc4f0002a24de355fb
shpilson <kovalenko.jora@gmail.com>
2021-05-15 16:08:22 +0200

    update

--- scripts/widgets/header.js
+++ scripts/widgets/header.js
@@ -2,7 +2,7 @@ const widgetSelector = document.querySelector(".header > div.header-wrapper > di
 
 function headerIcon() {
 widgetSelector.insertAdjacentHTML('afterbegin',`
-    <div class="header_wishlist">
+    <div class="header_login">
         <a class="d-none d-sm-block d-lg-none" href="https://store108758.sellavi.com/#" role="link">
         <i class="far fa-heart"></i>
         </a>
