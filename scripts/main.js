917fabf938e385098c3551fd2cc7c9dc48ec5493
shpilson <kovalenko.jora@gmail.com>
2021-05-15 15:50:01 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ import catalog from './catalog/catalog.js'; // Категория товаров
 import cart from './cart/cart.js'; // Смена кнопки "Купить" на "В корзину"
 import map from './map/map.js'; // Карта и контакты
 import send from './send/send.js'; // Форма обратной связи
-import header from './widgets/header.js'; // Виджеты в шапке
+import headerIcon from './widgets/header.js'; // Виджеты в шапке
 
 if (pages.main) {
     catalog();
