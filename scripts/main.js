537381332126093d31f2b09e82a4138323b46440
shpilson <kovalenko.jora@gmail.com>
2021-05-15 15:49:06 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -11,5 +11,5 @@ if (pages.main) {
     cart();
     map();
     send();
-    header();
+    headerIcon();
   }
--- scripts/widgets/header.js
+++ scripts/widgets/header.js
@@ -1,12 +1,11 @@
 const widgetSelector = document.querySelector("#undefined-sticky-wrapper > header > div.header-wrapper > div.container-fluid > div.header_widgets");
 
-function header() {
+function headerIcon() {
 widgetSelector.insertAdjacentHTML('beforeend',`
     <div class="header_test">
-        <a class="d-none d-lg-block" href="https://store108758.sellavi.com/login/" role="link">Авторизация</a>
-        <a class="d-none d-sm-block d-lg-none" href="https://store108758.sellavi.com/login/" role="link"><i class="far fa-user"></i></a>
-    </div>
-`)
-}
+        <a class="d-none d-lg-block" href="https://store108758.sellavi.com/#" role="link">Авторизация</a>
+        <a class="d-none d-sm-block d-lg-none" href="https://store108758.sellavi.com/#" role="link"><i class="far fa-user"></i></a>
+    </div>`
+)}
 
-export default header;
+export default headerIcon;
