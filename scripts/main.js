4e2e9e9ebb0c51fbcd3e357461200fcbda479fc5
shpilson <kovalenko.jora@gmail.com>
2021-05-15 15:41:14 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,12 @@ import catalog from './catalog/catalog.js'; // Категория товаров
 import cart from './cart/cart.js'; // Смена кнопки "Купить" на "В корзину"
 import map from './map/map.js'; // Карта и контакты
 import send from './send/send.js'; // Форма обратной связи
+import header from './widgets/header.js'; // Виджеты в шапке
 
 if (pages.main) {
     catalog();
     cart();
     map();
     send();
+    header();
   }
---
+++ scripts/widgets/header.js
@@ -0,0 +1,12 @@
+const widgetSelector = document.querySelector("#undefined-sticky-wrapper > header > div.header-wrapper > div.container-fluid > div.header_widgets");
+
+function header() {
+widgetSelector.insertAdjacentHTML('afterbegin',`
+    <div class="header_login">
+        <a class="d-none d-lg-block" href="https://store108758.sellavi.com/login/" role="link">Авторизация</a>
+        <a class="d-none d-sm-block d-lg-none" href="https://store108758.sellavi.com/login/" role="link"><i class="far fa-user"></i></a>
+    </div>
+`)
+}
+
+export default header;
