b4ae8c88843688abdb5e09d1ede754d9f60f9309
shpilson <kovalenko.jora@gmail.com>
2021-05-16 14:11:48 +0200

    update

--- scripts/footerLogo/footerLogo.js
+++ scripts/footerLogo/footerLogo.js
@@ -1,10 +1,14 @@
-const footerSelector = document.querySelector("body .footer .footer-meta .copyright")
+const footerSelector = document.querySelector("#home > div > footer > div.footer-meta > div > div")
 
 function footerLogo() {
     footerSelector.insertAdjacentHTML('afterbegin',`
-        <a href="http://ru.sellavi.com/" rel="nofollow" target="_blank" id="sellavi_new_logo" role="link">
-            <img data-src="https://raw.githubusercontent.com/shpilson/aquarel/main/styles/images/logo_sellavi.jpeg" alt="" class="img-fluid" src="https://raw.githubusercontent.com/shpilson/aquarel/main/styles/images/logo_sellavi.jpeg">
-        </a>`
+    <div class="col-sm-6">
+    <div class="copyright">
+    <a href="http://ru.sellavi.com/" rel="nofollow" target="_blank" id="sellavi_new_logo" role="link">
+    <img data-src="https://raw.githubusercontent.com/shpilson/aquarel/main/styles/images/logo_sellavi.jpeg" alt="" class="img-fluid" src="https://raw.githubusercontent.com/shpilson/aquarel/main/styles/images/logo_sellavi.jpeg">
+</a>
+    </div>
+</div>`
     )}
     
 export default footerLogo;
