16b6547304f88f96c386fa613648e7af55d7cf5a
shpilson <kovalenko.jora@gmail.com>
2021-05-16 14:27:02 +0200

    update

--- scripts/footerLogo/footerLogo.js
+++ scripts/footerLogo/footerLogo.js
@@ -2,9 +2,9 @@ const footerSelector = document.querySelector(".footer")
 
 function footerLogo() {
     footerSelector.insertAdjacentHTML('afterbegin',
-    `<div class="col-sm-6">
+    `
         <p>TEST</p>
-    </div>`
+    `
 )}
     
 export default footerLogo;
