e01469a768ba7c986cdc0a94dfe4ca36662a967c
shpilson <kovalenko.jora@gmail.com>
2021-05-17 12:28:40 +0200

    update

--- scripts/footerLogo/footerLogo.js
+++ scripts/footerLogo/footerLogo.js
@@ -3,10 +3,12 @@ const footerSelector = document.querySelector(".footer .footer-meta .container .
 function footerLogo() {
     footerSelector.insertAdjacentHTML('afterbegin', 
     `<div class="col-sm-6">
-        <div class="copyright">
-            <a href="http://ru.sellavi.com/" rel="nofollow" target="_blank" id="sellavi_new_logo" role="link">
-                <img data-src="https://shpilson.github.io/aquarel/styles/images/logo_vector.svg" alt="" class="img-fluid" src="https://shpilson.github.io/aquarel/styles/images/logo_vector.svg">
-            </a>
+        <div class="widget widget-categories">
+            <div class="copyright">
+                <a href="http://ru.sellavi.com/" rel="nofollow" target="_blank" id="sellavi_new_logo" role="link">
+                    <img data-src="https://shpilson.github.io/aquarel/styles/images/logo_vector.svg" alt="" class="img-fluid" src="https://shpilson.github.io/aquarel/styles/images/logo_vector.svg">
+                </a>
+            </div>
         </div>
     </div>`
 )}
