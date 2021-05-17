1b152b6ecae792ca9c16e15a4413053904f5e4f7
shpilson <kovalenko.jora@gmail.com>
2021-05-17 12:31:15 +0200

    update

--- scripts/footerSocial/footerSocial.js
+++ scripts/footerSocial/footerSocial.js
@@ -3,14 +3,16 @@ const socialIconSelector = document.querySelector(".footer .footer-widgets .cont
 function footerSocial() {
     socialIconSelector.insertAdjacentHTML('beforeend', 
     `<div class="col-md-3">
-        <h4 class="widget-title">Мы в соцсетях</h4>
-            <ul class="social-icons-new">
-            <li>
-                    <a href="instagram.com/aquarel" target="_blank" class="instagram" data-gtm_label="instagram" title="instagram_title" role="link">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                </li>
-         </ul>
+        <div class="widget widget-categories">
+            <h4 class="widget-title">Мы в соцсетях</h4>
+                <ul class="social-icons-new">
+                    <li>
+                        <a href="instagram.com/aquarel" target="_blank" class="instagram" data-gtm_label="instagram" title="instagram_title" role="link">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+        </div>
     </div>`
 )}
     
