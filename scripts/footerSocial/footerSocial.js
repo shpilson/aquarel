b8bf8050188462720c5b07d1060415039a5980cf
shpilson <kovalenko.jora@gmail.com>
2021-05-17 12:25:44 +0200

    update

--- scripts/footerSocial/footerSocial.js
+++ scripts/footerSocial/footerSocial.js
@@ -4,7 +4,7 @@ function footerSocial() {
     socialIconSelector.insertAdjacentHTML('beforeend', 
     `<div class="col-md-3">
         <h4 class="widget-title">Мы в соцсетях</h4>
-            <ul class="social-icons">
+            <ul class="social-icons-new">
             <li>
                     <a href="instagram.com/aquarel" target="_blank" class="instagram" data-gtm_label="instagram" title="instagram_title" role="link">
                         <i class="fab fa-instagram"></i>
